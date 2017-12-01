efd49ad8ef0b2d724c777fdfc00956c211b6e398
Alex Crimi <alexscrimi@gmail.com>
2017-11-30 21:07:14 -0800

    Day 1 solution

--- 2017/day1/solution.js
+++ 2017/day1/solution.js
@@ -1,5 +1,17 @@
 module.exports = (input, isPart2, isTest) => {
   let result = 0;
 
+  for (let i = 0; i < input.length; i++) {
+    let next = i + 1;
+    if (isPart2) {
+      next = i + input.length/2;
+    }
+    next %= input.length;
+    
+    if (input[i] === input[next]) {
+      result += parseInt(input[i]);
+    }
+  }
+
   return result;
 }
