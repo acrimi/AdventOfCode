96b617abc6be9056e69157322d527a149bad7ceb
Alex Crimi <alexscrimi@gmail.com>
2017-12-02 22:27:37 -0800

    Implements logic for determining day/year/part of selection

--- extension/app/content.js
+++ extension/app/content.js
@@ -1,10 +1,18 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // TODO parse day/year/part from url and dom
   var selection = window.getSelection().toString();
-  if (selection && selection.length) {
+  
+  var dayEl = $(window.getSelection().anchorNode.parentElement).parents('article.day-desc');
+  var previousArticles = dayEl.prevAll('article.day-desc');
+  
+  var urlMatch = location.href.match(/adventofcode.com\/(\d{4})\/day\/(\d+)/);
+
+  if (urlMatch && selection && selection.length) {
     chrome.runtime.sendMessage({
       name: request.name,
-      text: selection
+      selection: selection,
+      year: urlMatch[1],
+      day: urlMatch[2],
+      part: previousArticles.length == 0 ? 1 : 2
     });
   }
 });
