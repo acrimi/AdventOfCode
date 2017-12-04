0c87b635e8607a50949aca347c149606a66bdb56
Alex Crimi <alexscrimi@gmail.com>
2017-12-04 00:38:55 -0800

    Adds api method for submitting answer

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const https = require('https');
+const querystring = require('querystring');
 
 function loadSessionFromFile() {
   const path = __dirname + '/../session.txt';
@@ -37,5 +38,51 @@ exports.getInput = (year, day) => {
         resolve(input.trim());
       });
     });
+  });
+};
+
+exports.submit = (year, day, part, answer) => {
+  const session = process.env.AOC_SESSION || loadSessionFromFile();
+  if (!session) {
+    return Promise.resolve();
+  }
+
+  return new Promise(resolve => {
+    const data = querystring.stringify({
+      level: part,
+      answer: answer
+    });
+
+    const req = https.request({
+      method: 'POST',
+      hostname: 'adventofcode.com',
+      path: `/${year}/day/${day}/answer`,
+      headers: {
+        cookie: 'session='+session,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)
+      }
+    }, res => {
+      let response = '';
+
+      res.setEncoding('utf8');
+      res.on('data', chunk => {
+        response += chunk;
+      });
+
+      res.on('end', () => {
+        let message = response.match(/<article><p>(.*)<\/p><\/article>/);
+        if (message) {
+          message = message[1].replace(/<[^>]+>/g, '');
+          console.log(message);
+        } else {
+          console.log('Unparseable response for answer');
+        }
+        resolve();
+      });
+    });
+
+    req.write(data);
+    req.end();
   });
 }
