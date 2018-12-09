e06d149606ddc3547338d6186eb7b92d16ae774e
Alex Crimi <alexscrimi@gmail.com>
2018-12-09 02:45:12 -0800

    day 9 part 2

--- 2018/day9/solution.js
+++ 2018/day9/solution.js
@@ -4,6 +4,9 @@ module.exports = (input, isPart2, isTest) => {
   let [_, playerCount, lastMarble] = input.match(/(\d+) players; last marble is worth (\d+) points/);
   playerCount = +playerCount;
   lastMarble = +lastMarble;
+  if (!isTest && isPart2) {
+    lastMarble *= 100;
+  }
 
   const players = [];
   for (let i = 0; i < playerCount; i++) {
@@ -12,22 +15,33 @@ module.exports = (input, isPart2, isTest) => {
     });
   }
 
-  let marbles = [0];
-  let currentMarble = 0;
+  let currentMarble = {
+    value: 0
+  };
+  currentMarble.next = currentMarble;
+  currentMarble.prev = currentMarble;
 
   const insertMarble = (value) => {
-    let insertPoint = currentMarble + 2;
-    insertPoint %= marbles.length;
-    marbles.splice(insertPoint, 0, value);
-    currentMarble = insertPoint;
+    let leadNode = currentMarble.next;
+    let newNode = {
+      value: value,
+      next: leadNode.next,
+      prev: leadNode
+    };
+    newNode.next.prev = newNode;
+    leadNode.next = newNode;
+    currentMarble = newNode;
   };
 
   const takeMarble = () => {
-    let takePoint = currentMarble - 7 + marbles.length;
-    takePoint %= marbles.length;
-    let value = marbles[takePoint];
-    marbles.splice(takePoint, 1);
-    currentMarble = takePoint
+    let target = currentMarble;
+    for (let i = 0; i < 7; i++) {
+      target = target.prev;
+    }
+    let value = target.value;
+    target.prev.next = target.next;
+    target.next.prev = target.prev;
+    currentMarble = target.next;
     return value;
   }
 
