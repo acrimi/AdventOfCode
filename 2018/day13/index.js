6e6db966314e3438e56ca44d1ee8f0e43e676e17
Alex Crimi <alexscrimi@gmail.com>
2018-12-12 21:47:26 -0800

    2018 day 13 solution

---
+++ 2018/day13/index.js
@@ -0,0 +1,16 @@
+const runner = require('../../utils/runner.js');
+process.chdir(__dirname);
+
+runner.run({
+  input: null,
+  tests: [
+
+  ],
+  tests2: [
+
+  ],
+  separateLines: true,
+  lineCleanup: false,
+  columnDelimiter: false,
+  columnCleanup: false
+});
