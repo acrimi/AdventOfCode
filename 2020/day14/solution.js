9d74eb49267d6055bd833a1ff555c72e9eecc0e4
Alex Crimi <alexscrimi@gmail.com>
2020-12-14 00:21:52 -0800

    2020 day 14 pt 2 solution

--- 2020/day14/solution.js
+++ 2020/day14/solution.js
@@ -3,16 +3,46 @@ module.exports = (input, isPart2, isTest, testNumber) => {
 
   let andMask;
   let orMask;
-  const memory = [];
+  let floatMasks = [];
+  const memory = {};
+
+  function getPermutations(arr) {
+    if (!arr.length) {
+      return [BigInt(0)];
+    }
+    const shift = arr.shift();
+    let val = BigInt(1) << shift;
+    const perm = getPermutations(arr);
+    return perm.concat(perm.map(p => p | val));
+  }
 
   function setMasks(mask) {
-    andMask = BigInt('0b' + mask.replace(/X/g, '1'));
+    andMask = isPart2 ? BigInt('0b' + mask.replace(/0/g, '1').replace(/X/g, '0')) : BigInt('0b' + mask.replace(/X/g, '1'));
     orMask = BigInt('0b' + mask.replace(/X/g, '0'));
+    if (isPart2) {
+      const indices = [];
+      let index = -1;
+      while (true) {
+        index = mask.indexOf('X', index + 1);
+        if (index == -1) {
+          break;
+        }
+        indices.push(BigInt(35 - index));
+      }
+      floatMasks = getPermutations(indices);
+    }
   }
 
   function writeToMemory(address, value) {
-    value = BigInt(value) & andMask | orMask;
-    memory[address] = value;
+    if (!isPart2) {
+      value = value & andMask | orMask;
+      memory[address] = value;
+    } else {
+      address = address & andMask | orMask ;
+      for (const mask of floatMasks) {
+        memory[address | mask] = value;
+      }
+    }
   }
 
   for (const command of input) {
@@ -20,11 +50,11 @@ module.exports = (input, isPart2, isTest, testNumber) => {
       setMasks(command.match(/[\dX]+/)[0]);
     } else {
       const [_, addr, val] = command.match(/mem\[(\d+)\] = (\d+)/);
-      writeToMemory(addr, +val);
+      writeToMemory(BigInt(addr), BigInt(val));
     }
   }
 
-  result = memory.reduce((acc, i) => acc + (i || 0));
+  result = Object.values(memory).reduce((acc, i) => acc + i);
 
   return result;
 }
